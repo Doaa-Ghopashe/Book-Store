64d3192ed0385d8a29a6f4ff73039872405a44a1
omnia-khaled710 <omniakhaled887@gmail.com>
2023-05-24 01:14:42 +0300

    created bookModel and categoryModel

---
+++ controllers/bookController.js
@@ -0,0 +1,9 @@
+const bookModel=require('../models/book');
+
+const getAllBooks=async(req,res)=>{};
+const getOneBook=async(req,res)=>{};
+const addNewBook=async(req,res)=>{};
+const editBook=async(req,res)=>{};
+const deleteBook=async(req,res)=>{};
+
+module.exports={getAllBooks,getOneBook,editBook,deleteBook,addNewBook,getAllBooks}
