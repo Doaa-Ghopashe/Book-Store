c0ccfb1a4070ec627dabda2410e060259723a507
aya-mustafa <ayamustafa408@gmail.com>
2023-05-26 13:28:25 +0300

    login page is coded

--- front-end/angular/src/app/components/login/login.component.ts
+++ front-end/angular/src/app/components/login/login.component.ts
@@ -9,6 +9,40 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  constructor(private _router:Router)
+  {
+  }
+
+  regestrationForm:FormGroup = new FormGroup(
+    {
+      email :  new FormControl('',[Validators.required,Validators.email]),
+      password : new FormControl('',[Validators.required,Validators.pattern(/^[a-z]{1}[0-9]{3,8}/)]),
+    }
+  )
+
+  login()
+  {
+      // if(this.regestrationForm.status=='INVALID')
+      // {
+      //   return;
+      // }
+      // this.auth.signUp(this.regestrationForm.value).subscribe(
+      // {
+      //   next: res => {
+      //     this._router.navigateByUrl('/login')
+      //   },
+      //   error: err => alert(err.error.message),
+      //   complete: () => {
+      //     // let spin = document.getElementById("spin")!;
+      //     // spin.style.display = "none";
+      //   }
+      // })
+    }
+
+    navigate()
+    {
+      this._router.navigateByUrl('/user/register')
+    }
 
   
  
