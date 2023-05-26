f15973c6e0d7edc4adc103265f571180fe38a21e
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-05-26 09:07:06 +0300

    the new files has been added please pull them agian and add your work to

---
+++ models/category.js
@@ -0,0 +1,8 @@
+const mongoose =require("mongoose");
+
+const categorySchema=new mongoose.Schema({
+    Name:{type:String,required:true},
+})
+
+const categoryModel=mongoose.model('Category', categorySchema);
+module.exports=categoryModel;
