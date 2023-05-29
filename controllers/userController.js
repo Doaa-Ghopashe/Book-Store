749921adb8630e3e8d745775b940aa63bcf2e71d
monasrour <mona.omar.srour@gmail.com>
2023-05-29 18:28:22 +0300

    only Admin CAn dalete or Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const userModel = require('../models/user')
+const admin=require('../Middleware/admin')
+const auth = require('../Middleware/auth')
 
 function add(req, res){
     userModel.create({...req.body},(err,userData)=>{
@@ -31,37 +33,89 @@ function getById(req,res){
 
 function remove(req , res) {
     const {id}= req.params
-    userModel.findByIdAndDelete(id , (err)=>{
-        if (!err){
-            res.send( "deleted");
+    const {targ} = req.query
+    userModel.findById(id , (err ,data)=>{
+        user =data.isAdmin
+    if(!err){
+        if(user){
+            userModel.findById(targ , (err ,data)=>{
+                user2 =data.isAdmin
+                if(!err){
+                    if(!user2){
+                        userModel.findByIdAndDelete(targ , (err)=>{
+                            if (!err){
+                                res.send( "deleted");
+                            }
+                            else{
+                                console.log(err);
+                            }
+                         })
+
+                    } else {
+                        res.status(500).json("target is admin zayk")  
+                    }
+                } else{
+                    console.log(err);
+                }
+            })
+        } else {
+            res.status(500).json("you not admin")
         }
-        else{
-            console.log(err);
-        }
-     }) 
+    } else {
+       console.log(err);
+    }  
+    }) 
 }
+
 
 
 function edit(req , res) {
     const {id}= req.params
+    const {targ} = req.query
     let {firstName} = req.body
     let {lastName} =  req.body
     let {age} =  req.body
     let {email} =  req.body
     let {password} =  req.body
-    userModel.findByIdAndUpdate(id , {firstName , lastName , age ,email,password }  , (err)=>{
-        if (!err){
-            userModel.findById(id ,(err,data)=>{
-                if (!err) return res.status(201).json(data)
-                    console.log(err)
-                    res.status(500).json({Error : "DB_error"})
+    let {isAdmin} = req.body
+    userModel.findById(id , (err ,data)=>{
+        user =data.isAdmin
+    if(!err){
+        if(user){
+            userModel.findById(targ , (err ,data)=>{
+                user2 =data.isAdmin
+                if(!err){
+                    if(!user2){
+                        userModel.findByIdAndUpdate(targ , {firstName , lastName , age ,email,password,isAdmin }  , (err)=>{
+                            if (!err){
+                                userModel.findById(targ ,(err,data)=>{
+                                    if (!err) return res.status(201).json(data)
+                                        console.log(err)
+                                        res.status(500).json({Error : "DB_error"})
+                                })
+                            }
+                            else{
+                                console.log(err);
+                            }
+                         })
+
+                    } else {
+                        res.status(500).json("target is admin zayk")  
+                    }
+                } else{
+                    console.log(err);
+                }
             })
+        } else {
+            res.status(500).json("you not admin")
         }
-        else{
-            console.log(err);
-        }
-     })
+    } else {
+       console.log(err);
+    }  
+    }) 
 
 }
+
+
 
 module.exports = {add , list , getById , remove ,edit}
