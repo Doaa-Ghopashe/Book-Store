aabcee5772ba8b00fe90aee552583064f1a10f33
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-06-03 01:28:35 +0300

    the author details reorganized

--- Frontend/src/app/author/author-details/author-details.component.ts
+++ Frontend/src/app/author/author-details/author-details.component.ts
@@ -2,12 +2,6 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Author } from 'src/app/interfaces/author';
 import { AuthorService } from 'src/app/services/author.service';
-import { AvgrateService } from 'src/app/services/avgrate.service';
-import { BooksService } from 'src/app/services/books.service';
-import { CategoryService } from 'src/app/services/category.service';
-import { DecodetokenService } from 'src/app/services/decodetoken.service';
-import { ReservedbooksService } from 'src/app/services/reservedbooks.service';
-import { UserbooksService } from 'src/app/services/userbooks.service';
 @Component({
   selector: 'app-author-details',
   templateUrl: './author-details.component.html',
@@ -19,8 +13,7 @@ export class AuthorDetailsComponent {
   author!: Author;
   author_id!: string
 
-  constructor(private userbook: UserbooksService,
-    private Author: AuthorService,
+  constructor(private Author: AuthorService,
     private activetedrouter: ActivatedRoute) { }
 
   ngOnInit() {
