d3ecee4144d83ec266bfa54deb08125f378eefcd
Doaa Adel <48122971+Doaa-Ghopashe@users.noreply.github.com>
2023-06-02 14:01:04 +0300

    i include populate to the reserved books for the book id

--- Backend/controllers/reservedBooksController.js
+++ Backend/controllers/reservedBooksController.js
@@ -6,7 +6,7 @@ const reservedbooksModel = require("../models/reservedbooks"),
                 if (!err) return res.status(200).json(data);
 
                 throw new Error("notFound")
-            })
+            }).populate({path:"book_id"})
         } catch (err) {
             next({ status: errors[err.message].status, message: errors[err.message].errmessage })
         }
