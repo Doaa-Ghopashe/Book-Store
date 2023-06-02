301b33ff09c95d9555a454ce2763be9052e1b2d5
Doaa Adel <48122971+Doaa-Ghopashe@users.noreply.github.com>
2023-06-02 08:51:01 +0300

    the error has been resolved

--- Frontend/src/app/books/book-details/book-details.component.ts
+++ Frontend/src/app/books/book-details/book-details.component.ts
@@ -5,6 +5,8 @@ import { AuthorService } from 'src/app/services/author.service';
 import { AvgrateService } from 'src/app/services/avgrate.service';
 import { BooksService } from 'src/app/services/books.service';
 import { CategoryService } from 'src/app/services/category.service';
+import { DecodetokenService } from 'src/app/services/decodetoken.service';
+import { ReservedbooksService } from 'src/app/services/reservedbooks.service';
 import { UserbooksService } from 'src/app/services/userbooks.service';
 @Component({
   selector: 'app-book-details',
@@ -16,23 +18,33 @@ export class BookDetailsComponent {
   // @Input() status!:string;
   bookid!:string;
   book!:Book;
+  user_id!:string;
   no_of_readers:number=0
   // bookauthorId!:string
   // bookcategoryId!:string
   @Input() authorname!:string
   @Input() bookcategory!:string;
+  selectedopt!:string;
   constructor(private userbook:UserbooksService,
     private listedBooks:BooksService,
     private bookAuthor: AuthorService,
     private bookCategory: CategoryService,
     private activetedrouter:ActivatedRoute,
-    private avgrateservice:AvgrateService){}
+    private avgrateservice:AvgrateService,
+    private reservedbooksservice:ReservedbooksService,
+    private decodeservice : DecodetokenService){}
 
   ngOnInit(){
     this.bookid = this.activetedrouter.snapshot.params['id']
-
+    this.user_id = this.decodeservice.getDecodedAccessToken(localStorage.getItem('token')||'')?.['user_id']
     this.listedBooks.loadBooks();
-
+    this.reservedbooksservice.getResevedBooks(this.bookid,this.user_id).subscribe((res)=>{
+      document.querySelectorAll('option').forEach((option)=>{
+        if(option.value == res)
+          option.selected=true
+      })
+    })
+    
     this.listedBooks.getSpecificBook(this.bookid).
     subscribe((res:any)=> this.book = res);
 
@@ -54,16 +66,17 @@ export class BookDetailsComponent {
     this.bookCategory.getSpecificCategory().
     subscribe((res:any)=> this.bookcategory = res?.data?.category?.[0].Name );
 
-    this.avgrateservice.countAvgRate(this.bookid).subscribe()
-    this.avgrateservice.pubAvgRateVal.subscribe((res:number)=>{
+    this.avgrateservice.countAvgRate(this.bookid).subscribe();
+
+    this.avgrateservice.getAvgRate().subscribe((res:number)=>{
       this.avgrate = res
     })
     this.avgrateservice.pubReadersCount.subscribe((res:number)=>{
-      // console.log(res)
         this.no_of_readers =res
     })
+
   }
   changestatus(e:any){
-      this.userbook.reseveBook(e.target.value,this.bookid)
+      this.userbook.reseveBook(e.target.value,this.bookid,this.user_id)
   }
 }
