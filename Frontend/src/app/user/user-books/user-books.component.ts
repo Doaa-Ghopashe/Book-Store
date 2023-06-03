17db6e7bea07c111f244e2a23e8fb289f0e0ba43
omnia-khaled710 <omniakhaled887@gmail.com>
2023-06-03 22:10:40 +0300

    shevel

--- Frontend/src/app/user/user-books/user-books.component.ts
+++ Frontend/src/app/user/user-books/user-books.component.ts
@@ -17,40 +17,9 @@ export class UserBookComponent {
   
 
   currentContect:any[]=[];
-
-  // all:any[]=[
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3}
-  // ]
-  // currentRed:any[]=[
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  // ]
-  // reading:any[]=[
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  // ]
-  // wantToRead:any[]=[
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-  //   {img:"../../../assets/images/360_F_339709134_9FKW6cgJi0uosfWb2bD4zdcerTvRvNXv.jpg",name:"aya",auther:"moustafa",avg:"44",rate:3},
-    
-  // ]
   items: any[]=[]; // your list of items
   currentPage = 1; // start with the first page
-  itemsPerPage = 5; // show 5 items per page
+  itemsPerPage = 2; // show 5 items per page
 
 
 
